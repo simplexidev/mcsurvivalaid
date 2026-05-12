c792f652f18887e54766c4dc6b5bb6502652b1b9
Thomas Corwin <tacorwin@outlook.com>
2026-05-11 23:43:44 -0400

    Stop tracking compiled Bedrock script output

--- sources/behaviors/scripts/structures/structureLocator.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.showStructureLocatorMenu = showStructureLocatorMenu;
-async function showStructureLocatorMenu(player) {
-    player.sendMessage("Structure locator is not available yet.");
-}
